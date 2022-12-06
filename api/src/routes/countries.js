77e6796e0268e47b5bc815f94b13e3961a878907
JairOlgL <emiliojairo@hotmail.com>
2022-12-06 12:48:28 -0600

    Minor changes

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -7,14 +7,25 @@ countries.get('/', async(req, res) => {
     try {
         console.log(req.query)
         const countriesDB = req.query.name ? await Country.findAll({
-            attributes: ['name', 'flag'],
+            attributes: ['id', 'name', 'flag'],
             where: {
                 name: {[Op.iLike]: `%${req.query.name}%`}
             }
         }) : await Country.findAll({
             attributes: ['name', 'flag']
         })
-        res.status(200).json(countriesDB)
+        if(!countriesDB.length) throw `No se ha encontrado un país con el nombre ${req.query.name}`
+        else res.status(200).json(countriesDB)
+    } catch (error) {
+        res.status(404).json({error: error})
+    }
+})
+countries.get('/:idCountry', async(req, res) => {
+    try {
+        const {idCountry} = req.params;
+        const country = await Country.findOne({where: {id: idCountry}});
+        if(country) res.status(200).json(country)
+        else throw `No se ha encontrado ningún país con el ID ${idCountry}`
     } catch (error) {
         res.status(404).json({error: error})
     }
